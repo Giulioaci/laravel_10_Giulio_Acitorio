8af588be87f06dffcad37bc7b04cb291a6edb74c
Giulio Acitorio <giulioacitorio@yahoo.it>
2025-06-16 18:14:15 +0200

    esercizio fatto

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="card mb-3 imgg" style="width: 20rem;">
+                    <img src="{{$movie['img']}}" class="card-img-top cardimg" alt="{{$movie['title']}}">
+                    <div class="card-body">
+                    </div>
+                  </div>
